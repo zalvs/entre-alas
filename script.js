5d33dc589c088465c16b1b77b0cc22e9f7f40261
fchavonet <fchavonet@mail.fr>
2026-06-05 18:09:35 +0200

    Refactor UI with DaisyUI.

--- script.js
+++ script.js
@@ -1,70 +1,32 @@
-/*********************
-* RESPONSIVE WARNING *
-*********************/
+/******************************
+* RESPONSIVE WARNING BEHAVIOR *
+******************************/
 
 const responsiveWarning = document.getElementById("responsive-warning");
-// "true" if the site is optimized for responsive design, "false" if not.
+// Enable/disable responsive warning.
 const responsiveDesign = false;
+// Mobile width limit.
+const threshold = 768;
 
-// Show mobile warning if the user is on mobile and responsive-design is false.
-if (!responsiveDesign && window.innerWidth <= 768) {
-	responsiveWarning.classList.add("show");
+// Show or hide modal based on screen size.
+function checkResponsiveState() {
+  const small = window.innerWidth <= threshold;
+
+  if (!responsiveDesign && small) {
+    if (!responsiveWarning.open) {
+      responsiveWarning.showModal();
+      document.body.classList.add("overflow-hidden");
+    }
+  } else {
+    if (responsiveWarning.open) {
+      responsiveWarning.close();
+      document.body.classList.remove("overflow-hidden");
+    }
+  }
 }
 
+// Initial check.
+checkResponsiveState();
 
-/***********************
-* MODE TOGGLE BEHAVIOR *
-***********************/
-
-// Get elements that change with the mode.
-const toggleModeBtn = document.getElementById("toggle-mode-btn");
-const portfolioLink = document.getElementById("portfolio-link");
-const body = document.body;
-
-// Function to apply mode.
-function applyMode(mode) {
-	body.classList.remove("light-mode", "dark-mode");
-	body.classList.add(mode);
-
-	if (mode === "dark-mode") {
-		// Set dark mode styles.
-		toggleModeBtn.style.color = "rgb(245, 245, 245)";
-		toggleModeBtn.innerHTML = '<i class="bi bi-sun-fill"></i>';
-
-		portfolioLink.style.color = "rgb(245, 245, 245)";
-
-		responsiveWarning.style.backgroundColor = "rgb(2, 4, 8)";
-	} else {
-		// Set light mode styles.
-		toggleModeBtn.style.color = "rgb(2, 4, 8)";
-		toggleModeBtn.innerHTML = '<i class="bi bi-moon-stars-fill"></i>';
-
-		portfolioLink.style.color = "rgb(2, 4, 8)";
-
-		responsiveWarning.style.backgroundColor = "rgb(245, 245, 245)";
-	}
-}
-
-// Check and apply saved mode on page load
-let savedMode = localStorage.getItem("mode");
-
-if (savedMode === null) {
-	savedMode = "light-mode"; // Default mode.
-}
-applyMode(savedMode);
-
-// Toggle mode and save preference.
-toggleModeBtn.addEventListener("click", function () {
-	let newMode;
-
-	if (body.classList.contains("light-mode")) {
-		newMode = "dark-mode";
-	} else {
-		newMode = "light-mode";
-	}
-
-	applyMode(newMode);
-
-	// Save choice.
-	localStorage.setItem("mode", newMode);
-});
+// Real-time resize detection.
+window.addEventListener("resize", checkResponsiveState);
